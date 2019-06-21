29e4a52973dd28ff26f39bccfbc5dfcfb865a099
Dmytro Sheinin <dsprogr@gmail.com>
2019-06-21 10:49:43 +0200

    reformat

--- src/cli.ts
+++ src/cli.ts
@@ -1,25 +1,33 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
-const path = require('path');
-const program = require('commander');
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
+const path = require("path");
+const program = require("commander");
 
 clear();
-console.log(chalk.green(figlet.textSync('gpu-cli', { horizontalLayout: 'full' })));
+console.log(
+    chalk.green(figlet.textSync("gpu-cli", { horizontalLayout: "full" }))
+);
 
 program
-    .description('How to use google-play-uploader')
-    .option('-c, --config <file>', 'Configuration file for google-play-uploader')
-    .option('-t, --track <track>', 'Track for uploading')
-    .option('-a, --auth <authentication.json>', 'JSON file that contains private key and client email')
-    .option('-r, --recentChanges <message>', 'Recent changes message')
-    .option('-f, --apkFile <path/to.apk>', 'APK to upload')
-    .option('-o, --obbFile <path/to.obb>', 'OBB to upload')
+    .description("How to use google-play-uploader")
+    .option(
+        "-c, --config <file>",
+        "Configuration file for google-play-uploader"
+    )
+    .option("-t, --track <track>", "Track for uploading")
+    .option(
+        "-a, --auth <authentication.json>",
+        "JSON file that contains private key and client email"
+    )
+    .option("-r, --recentChanges <message>", "Recent changes message")
+    .option("-f, --apkFile <path/to.apk>", "APK to upload")
+    .option("-o, --obbFile <path/to.obb>", "OBB to upload")
     .parse(process.argv);
 
-console.log('you ordered a pizza with:');
+console.log("you ordered a pizza with:");
 if (!process.argv.slice(2).length) {
     program.outputHelp();
 }
