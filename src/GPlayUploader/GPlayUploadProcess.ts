d16079ad453add459324b6b629f8643e6031cc19
nico.rinck <nico.rinck@smartcrm.de>
2021-11-03 14:15:45 +0100

    rename and rewrite class for main upload process

---
+++ src/GPlayUploader/GPlayUploadProcess.ts
@@ -0,0 +1,49 @@
+import { GPlayUploaderConfig } from './GPlayUploaderConfig';
+import { ManifestParser } from 'GPlayUploader/UploadSteps/ManifestParser';
+import { GPlayEditCreator } from 'GPlayUploader/UploadSteps/GPlayEditCreator';
+import { GPlayUploadCompleter } from 'GPlayUploader/UploadSteps/GPlayUploadCompleter';
+import { GPlayFileUploader } from 'GPlayUploader/UploadSteps/GPlayFileUploader';
+import { BasicUploadParameters } from 'GPlayUploader/AndroidPublisherAPI/InterfaceTypes/BasicUploadParameters';
+import { ObbUploadParameters } from 'GPlayUploader/AndroidPublisherAPI/InterfaceTypes/ObbUploadParameters';
+import { TrackUpdateParameters } from 'GPlayUploader/AndroidPublisherAPI/InterfaceTypes/TrackUpdateParameters';
+import { AppUploadResult } from 'GPlayUploader/AndroidPublisherAPI/InterfaceTypes/AppUploadResult';
+
+export class GPlayUploadProcess {
+    constructor(
+        private readonly manifestParser: ManifestParser,
+        private readonly editCreator: GPlayEditCreator,
+        private readonly fileUploader: GPlayFileUploader,
+        private readonly uploadCompleter: GPlayUploadCompleter
+    ) {}
+
+    async start(uploaderConfig: GPlayUploaderConfig) {
+        try {
+            const packageName = await this.manifestParser.getPackageNameFromManifest(uploaderConfig.filePaths[0]);
+            const editId = await this.editCreator.getEditId(packageName);
+            const uploadParameters: BasicUploadParameters = { packageName, editId };
+            const uploadResults = await this.fileUploader.uploadAppPaths(uploaderConfig.filePaths, uploadParameters);
+
+            const oobUploadParameters: ObbUploadParameters = {
+                ...uploadParameters,
+                versionCodes: GPlayUploadProcess.getVersionCodeFromUploadResults(uploadResults)
+            };
+            await this.fileUploader.uploadObbFilePaths(uploaderConfig.obbFilePaths, oobUploadParameters);
+
+            const trackUpdateParameters: TrackUpdateParameters = {
+                ...oobUploadParameters,
+                track: uploaderConfig.track
+            };
+            await this.uploadCompleter.assignTrackAndReleaseNotes(trackUpdateParameters);
+            await this.uploadCompleter.commitChanges(uploadParameters);
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    private static getVersionCodeFromUploadResults(uploadResults: AppUploadResult[]): number[] {
+        return uploadResults.map((uploadResult: AppUploadResult) => {
+            return uploadResult.versionCode;
+        });
+    }
+}
