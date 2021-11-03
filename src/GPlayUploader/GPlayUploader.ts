e46eff20f87f95b27aae0cbc2d30968173f5bdc5
nico.rinck <nico.rinck@smartcrm.de>
2021-11-03 11:34:31 +0100

    update main class and config

--- src/GPlayUploader/GPlayUploader.ts
+++ src/GPlayUploader/GPlayUploader.ts
@@ -1,202 +1,79 @@
-import ApkReader from 'adbkit-apkreader';
-import { readManifest } from 'node-aab-parser';
-import { createReadStream } from 'fs';
-import { androidpublisher_v3, google } from 'googleapis';
 import { GPlayUploaderConfig } from './GPlayUploaderConfig';
-import { DH_UNABLE_TO_CHECK_GENERATOR } from 'constants';
-import Params$Resource$Edits$Apks$Upload = androidpublisher_v3.Params$Resource$Edits$Apks$Upload;
-import Androidpublisher = androidpublisher_v3.Androidpublisher;
-import Params$Resource$Edits$Bundles$Upload = androidpublisher_v3.Params$Resource$Edits$Bundles$Upload;
+import { ManifestParser } from 'GPlayUploader/UploadSteps/ManifestParser';
+import { ApkUploadResult, BundleUploadResult, UploadConfiguration } from 'GPlayUploader/Utilities/Types';
+import { GPlayApkUploader } from 'GPlayUploader/UploadSteps/GPlayApkUploader';
+import { GPlayBundleUploader } from 'GPlayUploader/UploadSteps/GPlayBundleUploader';
+import { GPlayObbUploader } from 'GPlayUploader/UploadSteps/GPlayObbUploader';
+import { GPlayEditCreator } from 'GPlayUploader/UploadSteps/GPlayEditCreator';
+import { GPlayUploadCompleter } from 'GPlayUploader/UploadSteps/GPlayUploadCompleter';
 
 export class GPlayUploader {
-    private _gPlayUploaderConfig: GPlayUploaderConfig;
+    constructor(
+        private readonly manifestParser: ManifestParser,
+        private readonly editCreator: GPlayEditCreator,
+        private readonly apkUploader: GPlayApkUploader,
+        private readonly bundleUploader: GPlayBundleUploader,
+        private readonly obbUploader: GPlayObbUploader,
+        private readonly uploadCompleter: GPlayUploadCompleter
+    ) {}
 
-    private packageName: string = '';
-    private versionCodes: string[] = [];
-    private editId: string = '';
-    private publisher: Androidpublisher;
-
-    constructor(gPlayUploaderConfig: GPlayUploaderConfig) {
-        this._gPlayUploaderConfig = gPlayUploaderConfig;
-    }
-
-    async start() {
+    async start(uploaderConfig: GPlayUploaderConfig) {
         try {
-            await this.parseManifest();
-            this.publisher = await this.authenticate();
-            await this.createEdit();
-            await this.uploadMultiplePaths(this._gPlayUploaderConfig.apkFilePaths, (apkFilePath) => {
-                return this.uploadSinglePath(apkFilePath);
-            });
-            await this.uploadMultiplePaths(this._gPlayUploaderConfig.obbFilePaths, (obbFilePath) => {
-                return this.uploadSinglePath(obbFilePath);
-            });
-            await this.assignTrackAndReleaseNotes();
-            await this.commitChanges();
+            const packageName = await this.manifestParser.getPackageNameFromManifest(uploaderConfig.filePaths[0]);
+            const editId = await this.editCreator.createEdit(packageName);
+            const uploadConfiguration: UploadConfiguration = { packageName, editId };
+
+            const uploadResults = await this.uploadFilePaths(uploaderConfig.filePaths, uploadConfiguration);
+            uploadConfiguration.versionCodes = GPlayUploader.getVersionCodeFromUploadResults(uploadResults);
+
+            await this.uploadObbFilePaths(uploaderConfig.obbFilePaths, uploadConfiguration);
+            await this.uploadCompleter.assignTrackAndReleaseNotes(uploaderConfig, uploadConfiguration);
+            await this.uploadCompleter.commitChanges(packageName, editId);
         } catch (error) {
             console.error(error);
             throw error;
         }
     }
 
-    async parseManifest() {
-        this._logger('> Parsing manifest');
-        this.packageName = await this.getPackageNameFromManifest(this._gPlayUploaderConfig.apkFilePaths[0]);
-        this._logger(`> Detected package name ${this.packageName}`);
-    }
-
-    async getPackageNameFromManifest(pathToPackage: string) {
-        if (this.isAABFilePath(pathToPackage)) {
-            const manifest = await readManifest(pathToPackage);
-            return manifest.packageName;
-        }
-        const manifest = await ApkReader.open(pathToPackage).readManifest();
-        return await manifest.package;
-    }
-
-    isAABFilePath(path: string): boolean {
-        return path.endsWith('.aab');
-    }
-
-    async authenticate(): Promise<androidpublisher_v3.Androidpublisher> {
-        this._logger('> Authenticating');
-        const client = await google.auth.getClient({
-            keyFile: this._gPlayUploaderConfig.authenticationPath,
-            scopes: 'https://www.googleapis.com/auth/androidpublisher'
+    private async uploadFilePaths(
+        filePaths: string[],
+        uploadConfiguration: UploadConfiguration
+    ): Promise<(ApkUploadResult | BundleUploadResult)[]> {
+        return await this.uploadMultiplePaths(filePaths, async (filePath) => {
+            return await this.uploadSinglePath(filePath, uploadConfiguration);
         });
+    }
 
-        const publisher = google.androidpublisher({
-            version: 'v3',
-            auth: client
+    private async uploadObbFilePaths(filePaths: string[], uploadConfiguration: UploadConfiguration): Promise<void> {
+        await this.uploadMultiplePaths(filePaths, (obbFilePath) => {
+            return this.obbUploader.upload(obbFilePath, uploadConfiguration);
         });
-
-        this._logger('> Authenticated succesfully');
-
-        return publisher;
     }
 
-    async createEdit() {
-        this._logger('> Creating edit');
-        const edit = await this.publisher.edits.insert({ packageName: this.packageName });
-        this.editId = edit.data.id;
-        this._logger(`> Created edit with id ${this.editId}`);
-        return edit;
-    }
-
-    async uploadMultiplePaths(pathsToUpload, uploadSinglePathFunction) {
-        const uploadQue: Promise<any>[] = [];
+    private async uploadMultiplePaths<T = any>(pathsToUpload, uploadSinglePathFunction): Promise<T[]> {
+        const uploadQue: Promise<T>[] = [];
 
         pathsToUpload.forEach((filePath) => {
-            const newUpload: Promise<any> = uploadSinglePathFunction(filePath);
+            const newUpload: Promise<T> = uploadSinglePathFunction(filePath);
             uploadQue.push(newUpload);
         });
 
         return Promise.all(uploadQue);
     }
 
-    async uploadSinglePath(filePath) {
-        if (this.isAABFilePath(filePath)) {
-            return this.uploadSingleAppBundle(filePath);
+    async uploadSinglePath(filePath, uploadConfiguration: UploadConfiguration) {
+        if (isAABFilePath(filePath)) {
+            return this.bundleUploader.upload(filePath, uploadConfiguration);
         }
-        return this.uploadSingleAPK(filePath);
-    }
-
-    async uploadSingleAPK(apkFilePath) {
-        this._logger(`> Uploading APK`);
-
-        const uploadApkConfig: Params$Resource$Edits$Apks$Upload = {
-            packageName: this.packageName,
-            editId: this.editId,
-            media: {
-                mimeType: 'application/vnd.android.package-archive',
-                body: createReadStream(apkFilePath)
-            }
-        };
-        const apkUpload = await this.publisher.edits.apks.upload(uploadApkConfig);
-        this.versionCodes.push(apkUpload.data.versionCode.toString());
-        this._logger(
-            `> Uploaded ${apkFilePath} with version code ${apkUpload.data.versionCode} and SHA1 ${apkUpload.data.binary.sha1}`
-        );
-        return apkUpload;
-    }
-
-    async uploadSingleAppBundle(aabFilePath) {
-        this._logger(`> Uploading App Bundle`);
-
-        const uploadAabConfig: Params$Resource$Edits$Bundles$Upload = {
-            packageName: this.packageName,
-            editId: this.editId,
-            media: {
-                body: createReadStream(aabFilePath)
-            }
-        };
-        const bundleUpload = await this.publisher.edits.bundles.upload(uploadAabConfig);
-        this.versionCodes.push(bundleUpload.data.versionCode.toString());
-        this._logger(
-            `> Uploaded ${aabFilePath} with version code ${bundleUpload.data.versionCode} and SHA1 ${bundleUpload.data.sha1}`
-        );
-        return bundleUpload;
-    }
-
-    async uploadSingleOBB(obbFilePath) {
-        this._logger(`> Uploading expansion file(s)`);
-        const obbUploadConfig: any = {
-            packageName: this.packageName,
-            editId: this.editId,
-            // TODO: find out a petter way to set up the right versioncodes.
-            apkVersionCode: this.versionCodes[0],
-            expansionFileType: 'main',
-            media: {
-                mimeType: 'application/octet-stream',
-                body: createReadStream(obbFilePath)
-            }
-        };
-        const obbUpload = await this.publisher.edits.expansionfiles.upload(obbUploadConfig);
-        return obbUpload;
-    }
-
-    async assignTrackAndReleaseNotes() {
-        this._logger(`> Assigning APK to ${this._gPlayUploaderConfig.track} track`);
-
-        const newTrackConfig = {
-            packageName: this.packageName,
-            editId: this.editId,
-            track: this._gPlayUploaderConfig.track,
-            resource: {
-                track: this._gPlayUploaderConfig.track,
-                releases: [
-                    {
-                        versionCodes: this.versionCodes,
-                        releaseNotes: this.getReleaseNotes(),
-                        status: 'completed'
-                    }
-                ]
-            }
-        };
-
-        const newTrack: any = await this.publisher.edits.tracks.update(newTrackConfig);
-        this._logger(`> Assigned APK to ${newTrack.track} track`);
-        return newTrack;
-    }
-
-    getReleaseNotes() {
-        const newReleaseNotes = [];
-        this._logger(`> Adding release notes`);
-
-        return newReleaseNotes;
-    }
-
-    async commitChanges() {
-        this._logger('> Commiting changes');
-        const commitedChanges = await this.publisher.edits.commit({
-            editId: this.editId,
-            packageName: this.packageName
+        return this.apkUploader.upload(filePath, {
+            ...uploadConfiguration,
+            mimeType: 'application/vnd.android.package-archive'
         });
-        this._logger('> Commited changes');
-        return commitedChanges;
     }
 
-    _logger(message: string) {
-        console.log(message);
+    private static getVersionCodeFromUploadResults(uploadResults: (ApkUploadResult | BundleUploadResult)[]): number[] {
+        return uploadResults.map((uploadResult: ApkUploadResult | BundleUploadResult) => {
+            return uploadResult.versionCode;
+        });
     }
 }
